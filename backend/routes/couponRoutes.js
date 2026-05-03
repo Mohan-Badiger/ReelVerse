5df0a9730a3de6418c109b50552df5b17cafa226
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-05-03 23:14:02 +0530

    a

--- backend/routes/couponRoutes.js
+++ backend/routes/couponRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
     createCoupon,
     getCoupons,
+    getActiveCoupons,
     validateCoupon,
     deleteCoupon,
 } from '../controllers/couponController.js';
@@ -11,6 +12,7 @@ import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').get(protectAdmin, getCoupons).post(protectAdmin, createCoupon);
+router.route('/active').get(getActiveCoupons);
 router.route('/validate').post(protect, validateCoupon);
 router.route('/:id').delete(protectAdmin, deleteCoupon);
 
