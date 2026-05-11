f427ab52a9899e19fcd481aaf75c55e73287705f
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-05-11 11:02:53 +0530

    4

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
+import dns from 'dns';
 
 const connectDB = async () => {
+  dns.setServers(['8.8.8.8', '8.8.4.4']);
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected`);
